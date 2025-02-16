33d166398cc8bc9a81aaa7a1ae576149901baf0c
Dave Williams <dave@dave.io>
2025-02-16 19:13:51 +0000

    add a few modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: {
     enabled: true,
   },
-  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxt/content",
+    "@nuxt/icon",
+    "@nuxt/ui",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@primevue/nuxt-module",
+    "@vee-validate/nuxt",
+    "@vueuse/nuxt",
+    "shadcn-nuxt",
+  ],
+  // shadcn: {
+  //   prefix: "shad-",
+  // },
 });
